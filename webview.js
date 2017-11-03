243bc29731ed8796d143207a0eaa3d9fc7046ac6
Jonas H <jonhil@users.noreply.github.com>
2017-11-03 19:59:11 +0100

    Add files via upload

---
+++ webview.js
@@ -0,0 +1,16 @@
+// orat.io integration
+module.exports = (Franz) => {
+  function getMessages() {
+    let direct = 0;
+    let indirect = 0;
+    const FranzData = document.querySelector('#FranzMessages').dataset;
+    if (FranzData) {
+      direct = FranzData.direct;
+      indirect = FranzData.indirect;
+    }
+
+    Franz.setBadge(direct, indirect);
+  }
+
+  Franz.loop(getMessages);
+}
